1007bc5d513c1c9ea61836068c3f2f4f52b235f1
bhlynskyExadel <bhlynskyi@exadel.com>
2021-09-22 17:46:51 +0300

    added "add user to group" endpoint

--- routes/group.js
+++ routes/group.js
@@ -31,4 +31,31 @@ router.get('/:userId', async (req, res) => {
     }
 });
 
+//add user/users to group
+
+router.put('/addusers/:groupId', async (req, res) => {
+    try {
+        const groupId = req.params.groupId;
+        const newUsers = req.body;
+
+        let group = await Group.findById(groupId);
+        group.members = group.members.concat(newUsers);
+
+        console.log(group.members);
+
+        !group && res.status(404).json({ message: 'Group no found' });
+
+        const updatedGroup = await Group.findOneAndUpdate(
+            { id: groupId },
+            group
+        );
+
+        !updatedGroup && res.status(400).json({ message: "Can't save group" });
+
+        res.status(200).json(updatedGroup);
+    } catch (err) {
+        res.status(500).json({ message: `Error ${err}` });
+    }
+});
+
 module.exports = router;
